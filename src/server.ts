11e0062d2377b4504fb67e9517a23085b7a5e403
Mohammed Ahmed Abdus <mdahmed.abdus@gmail.com>
2024-09-25 16:59:14 +0530

    feat: MongoDB setup

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import { error } from 'console'
 import app from './app'
 import config from './config/config'
+import dbService from './service/dbService'
 import logger from './utils/logger'
 
 const server = app.listen(config.PORT)
 
-;(() => {
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+;(async () => {
   try {
-    // TODO: database connection
+    const conn = await dbService.connect()
+    logger.info('Database connection', { meta: { connectionName: conn.name } })
     logger.info('Server is running...', { meta: { PORT: config.PORT, SERVER_URL: config.SERVER_URL } })
   } catch {
     logger.error('Server error. Server is not running.', { meta: error })
