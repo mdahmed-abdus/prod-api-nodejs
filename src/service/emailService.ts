b1129cb9a0e9d5d1681f1cf094fd8bc866cb6f9f
Mohammed Ahmed Abdus <mdahmed.abdus@gmail.com>
2024-10-04 11:16:21 +0530

    refactor: Remove unnecessary code

--- src/service/emailService.ts
+++ src/service/emailService.ts
@@ -5,15 +5,11 @@ const resend = new Resend(config.EMAIL_SERVICE_API_KEY)
 
 export default {
   sendMail: async (to: string[], subject: string, text: string) => {
-    try {
-      await resend.emails.send({
-        from: 'prod-api-nodejs / MA2 <onboarding@resend.dev>',
-        to,
-        subject,
-        text
-      })
-    } catch (error) {
-      throw error
-    }
+    await resend.emails.send({
+      from: 'prod-api-nodejs / MA2 <onboarding@resend.dev>',
+      to,
+      subject,
+      text
+    })
   }
 }
